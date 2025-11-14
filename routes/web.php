20b9f5b4421adf112b10cb730c652f4aea0e8f96
Luan Neumann Dev <luan.neumann.dev@gmail.com>
2025-11-13 22:17:00 -0300

    chore: initial view of email lists

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+
+use App\Http\Controllers\EmailListController;
+use App\Http\Controllers\ProfileController;
 
 Route::view('/', 'welcome');
 
@@ -11,6 +13,9 @@ Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/email-list', [EmailListController::class, 'index'])->name('email-list.index');
+    Route::get('/email-list/create', [EmailListController::class, 'create'])->name('email-list.create');
 });
 
 require __DIR__.'/auth.php';
