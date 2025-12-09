0c970da919132016a7121ab1e1ce273cc19574f5
Luan Neumann Dev <luan.neumann.dev@gmail.com>
2025-12-09 20:43:18 -0300

    chore: create seeders and factories

---
+++ database/seeders/EmailListSeeder.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\EmailList;
+use App\Models\Subscriber;
+use Illuminate\Database\Seeder;
+
+class EmailListSeeder extends Seeder
+{
+    public function run(): void
+    {
+        EmailList::factory()->count(50)->create()
+            ->each(function (EmailList $list) {
+                Subscriber::factory()
+                    ->count(rand(50, 200))
+                    ->create(['email_list_id' => $list->id]);
+            });
+    }
+}
