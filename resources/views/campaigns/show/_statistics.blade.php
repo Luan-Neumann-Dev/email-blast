d90a5b92e6cf254ae6c93109d24fd2c4b1fd79d4
Luan Neumann Dev <luan.neumann.dev@gmail.com>
2026-02-03 20:55:57 -0300

    chore: statistics view

--- resources/views/campaigns/show/_statistics.blade.php
+++ resources/views/campaigns/show/_statistics.blade.php
@@ -1,3 +1,12 @@
-<x-card>
-    Statistics
-</x-card>
+<div class="flex flex-col gap-4">
+    <x-alert success no-icon :title="__('Sua campanha foi enviada para 10 assinantes da primeira lista.')"/>
+
+    <div class="grid grid-cols-3 gap-5">
+        <x-dashboard.card heading="01" subheading="{{ __('Opens') }}" />
+        <x-dashboard.card heading="02" subheading="{{ __('Unique Opens') }}" />
+        <x-dashboard.card heading="20%" subheading="{{ __('Open Rate') }}" />
+        <x-dashboard.card heading="0" subheading="{{ __('Clicks') }}" />
+        <x-dashboard.card heading="0" subheading="{{ __('Unique Clicks') }}" />
+        <x-dashboard.card heading="15%" subheading="{{ __('Clicks Rate') }}" />
+    </div>
+</div>
