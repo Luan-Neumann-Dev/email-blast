56b8cf1b93bedadf838ece5f19bd54ef3d0abf0c
Luan Neumann Dev <luan.neumann.dev@gmail.com>
2026-02-04 21:19:24 -0300

    chore: trasnfer logic for scopes

--- app/Http/Controllers/CampaignController.php
+++ app/Http/Controllers/CampaignController.php
@@ -44,17 +44,15 @@ class CampaignController extends Controller
 
         $query = $campaign
             ->mails()
-            ->with('subscriber')
-            ->when($search, fn(Builder $query) => $query
-                ->whereHas('subscriber', fn(Builder $query) => $query
-                    ->where('name', 'like', "%$search%")
-                    ->orWhere('email', 'like', "%$search%")
-                )
-            )
-            ->orderByDesc('openings')
+            ->when($what == 'statistics', fn(Builder $query) => $query->statistics())
+            ->when($what == 'open', fn(Builder $query) => $query->openings($search))
+            ->when($what == 'clicked', fn(Builder $query) => $query->clicks($search))
             ->simplePaginate(5)
             ->appends(compact('search'));
 
+        if ($what == 'statistics') {
+            $query = $query->first()->toArray();
+        }
 
         return view('campaigns.show', compact('campaign', 'what', 'search', 'query'));
     }
