2dd3125e7d2d2c3f19e03b0d589d156f5835f93a
Luan Neumann Dev <luan.neumann.dev@gmail.com>
2026-03-05 22:11:29 -0300

    feat: create delete test for subscribers

---
+++ tests/Feature/EmailList/Subscribers/DeleteTest.php
@@ -0,0 +1,21 @@
+<?php
+
+
+use App\Models\Subscriber;
+use function Pest\Laravel\{assertSoftDeleted, delete};
+
+it('should be able to delete a subscriber from a list', function () {
+    login();
+    $subscriber = Subscriber::factory()->create();
+
+    delete(
+        route('subscribers.destroy', [
+            'emailList' => $subscriber->emailList,
+            'subscriber' => $subscriber
+        ])
+    )->assertRedirect();
+
+    assertSoftDeleted('subscribers', [
+        'id' => $subscriber->id
+    ]);
+});
